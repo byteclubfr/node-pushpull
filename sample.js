657e705842e85cfd68b2bb03994d7c466b9ac6d2
Nicolas Chambrier <naholyr@gmail.com>
2015-02-19 23:25:29 +0100

    Add sample

---
+++ sample.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var options = { "queue": "sample_queue" };
+
+// Pullers
+
+var Pull = require("./").Pull;
+
+var worker1 = new Pull(options);
+worker1.on("data", function (data) {
+  console.log("received data (1)", data);
+});
+
+var worker2 = new Pull(options);
+worker2.on("data", function (data) {
+  console.log("received data (2)", data);
+});
+
+// Pusher
+
+var Push = require("./").Push;
+
+var sender = new Push(options);
+
+for (var i = 0; i < 20; i++) {
+  sender.emit("data", "data " + i);
+}
